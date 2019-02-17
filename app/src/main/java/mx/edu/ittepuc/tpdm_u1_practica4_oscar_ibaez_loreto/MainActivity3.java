99de5450788f2cbeb7976bd74ab39992accb9af9
oscarilo <osabibanezlo@ittepic.edu.mx>
2019-02-16 19:16:35 -0700

    Practica 4 Oscar Ibañez Loreto

---
+++ app/src/main/java/mx/edu/ittepuc/tpdm_u1_practica4_oscar_ibaez_loreto/MainActivity3.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepuc.tpdm_u1_practica4_oscar_ibaez_loreto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
